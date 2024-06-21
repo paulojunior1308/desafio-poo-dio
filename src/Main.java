1e6af900c2e7302f44e7b015f7cd72063b66a54b
paulojunior1308 <pauloesjr2@gmail.com>
2024-06-21 15:51:53 -0300

    Banco em Python

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,32 @@
-import br.com.dio.desafio.dominio.*;
-
-import java.time.LocalDate;
+import java.util.*;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
-        Curso curso1 = new Curso();
-        curso1.setTitulo("Curso Java");
-        curso1.setDescricao("Descrição curso Java");
-        curso1.setCargaHoraria(8);
-
-        Curso curso2 = new Curso();
-        curso2.setTitulo("Curso C#");
-        curso2.setDescricao("Descrição curso C#");
-        curso2.setCargaHoraria(4);
-
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria de Java");
-        mentoria.setDescricao("Descrição mentoria java");
-        mentoria.setData(LocalDate.now());
-
-       /* System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
-
-        Bootcamp bootcamp = new Bootcamp();
-        bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
-        bootcamp.getConteudos().add(curso1);
-        bootcamp.getConteudos().add(curso2);
-        bootcamp.getConteudos().add(mentoria);
-
-        Dev dev = new Dev();
-        dev.setNome("Paulo");
-        dev.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos " + dev.getConteudosInscritos());
-        dev.progredir();
-        dev.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos " + dev.getConteudosInscritos());
-        System.out.println("Conteúdos Concluidos " + dev.getConteudosConcluidos());
-        System.out.println("XP: " + dev.calcularTotalXp());
-
-        System.out.println("------------");
-
-        Dev dev2 = new Dev();
-        dev2.setNome("João");
-        dev2.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos " + dev2.getConteudosInscritos());
-        dev2.progredir();
-        dev2.progredir();
-        dev2.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos " + dev.getConteudosInscritos());
-        System.out.println("Conteúdos Concluidos " + dev2.getConteudosConcluidos());
-        System.out.println("XP: " + dev2.calcularTotalXp());
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int res = n;
+        int cnt = 1;
+        int tmp = n;
+        
+        n--;
+        
+        while (n > 0) {
+            if (cnt % 4 == 1) {
+                res *= n;
+            } else if (cnt % 4 == 2) {
+                res /= n;
+            } else if (cnt % 4 == 3) {
+                res += n;
+            } else {
+                res -= n;
+            }
+            
+            cnt++;
+            n--;
+        }
+        
+        System.out.println(res);
     }
+   
 }
